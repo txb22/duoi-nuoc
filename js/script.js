26fbede18dc6e127867612d19155f269e7e14b4d
txb22 <tranxuanbacit@gmail.com>
2021-09-06 16:17:40 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -13,7 +13,40 @@ btn.on('click', function (e) {
     $('html, body').animate({ scrollTop: 0 }, '300');
 });
 
+jQuery(document).ready(
+    function () {
+        menuMobile();
+    });
 
+function menuMobile() {
+    $("#bt-menu-mobile").click(function () {
+        $("#wrapper-menu-mobile").css({ left: 0, opacity: 0 });
+        $("#wrapper-menu-mobile").animate({ opacity: 1 }, 200);
+        $("#wrapper-menu-mobile .wrapper").animate({ right: "0" }, 500);
+        jQuery(document.body).css('overflow', 'hidden');
+        console.log('up')
+        return false;
+    });
+
+    $("#bt-fechar-menu").click(function () {
+        closeMenuMobile();
+        return false;
+    });
+
+    $(".block-menu-mobile").mousedown(function () {
+        closeMenuMobile();
+    });
+}
+
+function closeMenuMobile() {
+    $("#wrapper-menu-mobile .wrapper").animate({ right: "-100%" }, 300, function () {
+        $("#wrapper-menu-mobile").animate({ opacity: 0 }, 200, function () {
+            $("#wrapper-menu-mobile").css({ left: "100%", opacity: 1 });
+            jQuery(document.body).css('overflow', 'auto');
+            console.log('down')
+        });
+    });
+}
 document.getElementById("click").addEventListener("click", function () {
     document.getElementById("search-field").classList.toggle("form-active");
 });
