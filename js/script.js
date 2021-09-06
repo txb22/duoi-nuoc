0b657362cabd10df44523046ac5cba9d3f4a2b33
txb22 <tranxuanbacit@gmail.com>
2021-09-06 10:42:14 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+
+document.getElementById("click").addEventListener("click", function () {
+    document.getElementById("search-field").classList.toggle("form-active");
+});
